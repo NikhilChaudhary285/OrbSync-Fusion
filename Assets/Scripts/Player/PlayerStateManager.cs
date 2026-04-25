b43ecc014f386a2d10eb1b60a2ce93eff83a7020
NikhilChaudhary285 <nikhilchaudhary285@gmail.com>
2026-04-25 16:06:31 +0530

    Phase 3: Create orb prefab with visual, material, and interaction component

--- Assets/Scripts/Player/PlayerStateManager.cs
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -3,7 +3,16 @@ using Fusion;
 
 public class PlayerStateManager : MonoBehaviour
 {
+    public static PlayerStateManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+    }
+
     public void SpawnFreshPlayer(PlayerRef player) { }
     public void SpawnAndRestorePlayer(PlayerRef player, PlayerSaveData data) { }
     public void SavePlayerData(PlayerRef player) { }
+    public void AddScore(int score) { }
 }
